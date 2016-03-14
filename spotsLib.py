e870188030788c3c4d89e15ad08ae88f7f083945
mt-christo <slepnev@yandex.ru>
2016-03-14 22:37:17 +0300

    Update spotsLib.py

--- spotsLib.py
+++ spotsLib.py
@@ -3,6 +3,34 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
+class Spot(object):
+    def __init__(self, x1, y1, r, col, draw_f):
+        self.x = x1
+        self.y = y1
+        self.size = r
+        self.color = col
+        self.draw_f = draw_f
+        
+    def recolor(self, new_col):
+            self.color = new_col                
+        
+    def draw(self):
+        self.draw_f(self.x,self.y,self.size,self.color)
+        
+    def behave(self, letter, action):
+        fig, ax = plt.subplots()
+        fig.canvas.mpl_connect('key_press_event', 
+                               lambda event, self: action() if event.key == letter else false)
+
+class Spots(object):
+    def __init__(self):
+        self.spots = []
+        
+    def draw(self):
+        for s in self.spots:
+            s.draw()
+            
+
 def circa(x, y, r, col):
     circle = plt.Circle((x, y), radius=r, fc=col)
     plt.gca().add_patch(circle)
