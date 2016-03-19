0e3eb23ebc84bac7cd53963019d54bbe0e209482
mt-christo <slepnev@yandex.ru>
2016-03-19 18:08:33 +0300

    Update spotsTest.py

--- spotsTest.py
+++ spotsTest.py
@@ -1,5 +1,7 @@
 from spotsLib import *
 
-s = Spot(patches.Circle((5,5), radius=1, color='red'), plt.figure(), ['g', 'w'], ['green', 'blue'])
+s = Spot(patches.Circle((5,5), radius=1, color='red'), plt.figure())
+s.add_color('w','red')
+s.add_color('q','blue')
 
 s.draw()
