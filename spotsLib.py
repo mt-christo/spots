52af3db0542bf71201a79298533d674884a9eeb6
mt-christo <slepnev@yandex.ru>
2016-03-19 17:43:10 +0300

    Update spotsLib.py

--- spotsLib.py
+++ spotsLib.py
@@ -2,51 +2,39 @@ from __future__ import print_function
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 class Spot(object):
-    def __init__(self, x1, y1, r, col, draw_f):
-        self.x = x1
-        self.y = y1
-        self.size = r
-        self.color = col
-        self.draw_f = draw_f
+    def __init__(self, patch, figure, letters, colors):
+        self.patch = patch
+        self.figure = figure
+        self.subplot = figure.add_subplot(111)
+        self.letters = letters
+        self.colors = colors
+        self.figure.canvas.mpl_connect('key_press_event', self.press)
         
-    def recolor(self, new_col):
-            self.color = new_col                
-        
+    def press(self, event):
+        for i, p in enumerate(self.letters):
+            if event.key == p:
+                print('press', p)
+                self.patch.set_color(self.colors[i])
+                self.figure.canvas.draw()
+         
     def draw(self):
-        self.draw_f(self.x,self.y,self.size,self.color)
-        
-    def behave(self, letter, action):
-        fig, ax = plt.subplots()
-        fig.canvas.mpl_connect('key_press_event', 
-                               lambda event, self: action() if event.key == letter else false)
-
+        self.subplot.add_patch(self.patch)
+        plt.axis('scaled')
+        plt.show()
+ 
 class Spots(object):
     def __init__(self):
         self.spots = []
         
-    def draw(self):
-        for s in self.spots:
-            s.draw()
-            
-
-def circa(x, y, r, col):
-    circle = plt.Circle((x, y), radius=r, fc=col)
-    plt.gca().add_patch(circle)
-
-def showKirill(drawFunc):
-    plt.axes()
-    drawFunc()
-    plt.axis('scaled')
-    plt.show()
+    #def draw(self):
+        #for s in self.spots:
     
-def myDraw():
-    circa(0,0,10,'red')    
-
 def press(event):
     print('press', event.key)
-    sys.stdout.flush()
+    #sys.stdout.flush()
     #if event.key == 'x'
 
 #fig, ax = plt.subplots()
